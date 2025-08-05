2a416b13a501911df98f2abd8402020a790d3402
jengahmedeyan <58713713+jengahmedeyan@users.noreply.github.com>
2025-08-05 22:48:47 +0000

    Register helpers for source

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -23,7 +23,10 @@ export class HeadlineHubBot {
     this.bot.onText(/\/help/, this.handlers.handleHelp.bind(this.handlers));
     this.bot.onText(/\/latest/, this.handlers.handleLatest.bind(this.handlers));
     this.bot.onText(/\/fetch/, this.handlers.handleFetch.bind(this.handlers));
-    
+    this.bot.onText(/\/sources/, this.handlers.handleSources.bind(this.handlers));
+    this.bot.onText(/\/source (.+)/, this.handlers.handleSources.bind(this.handlers));
+  
+        
     this.bot.on("callback_query", this.handlers.handleCallback.bind(this.handlers));
 
     this.bot.on("polling_error", (error) => {
