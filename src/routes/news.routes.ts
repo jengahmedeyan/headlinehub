32e2726c63229670c0ed1ae4e205a2e2d7b4467f
jengahmedeyan <58713713+jengahmedeyan@users.noreply.github.com>
2025-08-11 21:09:17 +0000

    update routes

--- src/routes/news.routes.ts
+++ src/routes/news.routes.ts
@@ -7,10 +7,14 @@ const newsController = new NewsController();
 router.get("/", newsController.getAllNews);
 router.get("/latest", newsController.getLatestNewsArticles);
 router.get("/search", newsController.searchNews);
+
 router.get("/categories", newsController.getAvailableCategories);
 router.get("/sources", newsController.getAvailableSources);
-router.get("/health", newsController.getHealthStatus);
 router.get("/source/:source", newsController.getNewsBySource);
+
+router.get("/health", newsController.getHealthStatus);
+router.get("/test-scrape", newsController.dummyScraper);
+
 router.get("/:id", newsController.getArticleById);
 
 export { router as newsRoutes };
